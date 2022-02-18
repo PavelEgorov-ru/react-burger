ab025c7420e943a069e4152b3cb22aea1627df66
Nikita Stroganov <nstroganov@adsterra.com>
2022-02-18 16:12:14 +0300

    fix types

--- src/services/reducers/elements/elementsSlice.ts
+++ src/services/reducers/elements/elementsSlice.ts
@@ -1,68 +1,41 @@
-import { createSlice, nanoid } from '@reduxjs/toolkit';
+import { createSlice, nanoid } from "@reduxjs/toolkit";
 
-/* здесь интерфейс для начального стейта. Я так понимаю, он типизирует и стейт
-который прилетает в редьюсер*/
-interface IStateConstructor {
-  bun: {} | IElement | IIngredient,
-  elements: [] | Array<IElement>,
-  isElements: boolean
-}
-
-// интерфейс ингредиента, который прилетает для контсруктора, перед получением
-// уникального  id
-interface IIngredient {
-  _id: string,
-  name: string,
-  type: string,
-  proteins: number,
-  fat: number,
-  carbohydrates: number,
-  calories: number,
-  price: number,
-  image: string,
-  image_mobile: string,
-  image_large: string,
-  __v: number,
-}
-
-// интерфейс элемента, который попадает в массив для конструктора
-interface IElement extends IIngredient {
-  uid: string
-}
+import type { PayloadAction } from "@reduxjs/toolkit";
+import type { IStateConstructor, IIngredient, IElement } from "./types";
 
 const initialStateConstructor: IStateConstructor = {
   bun: {},
   elements: [],
   isElements: false,
-}
+};
 
 const elementsSlice = createSlice({
-  name: 'elements',
+  name: "elements",
   initialState: initialStateConstructor,
   reducers: {
-    postBun(state, action) {    
-      state.bun = action.payload
-      state.isElements=true      
+    postBun(state, action) {
+      state.bun = action.payload;
+      state.isElements = true;
     },
     postElement: {
-      reducer: (state, action) => {
-        state.elements = [...state.elements, action.payload]
+      reducer: (state, { payload }: PayloadAction<IElement>) => {
+        state.elements.push(payload);
       },
-      // указал тип для аргумента text
-      // сбда же прилетает ингредиент без уникального id
-      prepare: (text: IIngredient) => {
-        const uid = nanoid()
-        return { payload: { uid, ...text } }
+      prepare: (ingredient: IIngredient) => {
+        const uid = nanoid();
+        return { payload: { uid, ...ingredient } };
       },
     },
     deleteElement(state, action) {
-      state.elements = state.elements.filter((element) => element.uid !== action.payload)
+      state.elements = state.elements.filter(
+        (element) => element.uid !== action.payload
+      );
     },
     newOrderElements(state, action) {
-      state.elements = action.payload
-    }
-  }
-})
+      state.elements = action.payload;
+    },
+  },
+});
 
 export const elementsReducers = elementsSlice.reducer;
-export const elementsActions = elementsSlice.actions
+export const elementsActions = elementsSlice.actions;
