41ddcf366f33f995c175aaf0784f6a28e18bcdba
PavelEgorov-ru <pkotofffff@yandex.ru>
2022-03-07 15:04:54 +0300

    экспортировал экшены

--- src/services/reducers/index.js
+++ src/services/reducers/index.js
@@ -3,6 +3,10 @@ import { ingredientsReducers } from './ingredientsSlice';
 import { elementsReducers } from './elementsSlice';
 import { ingredientReducers } from './ingredientSlice';
 import { orderReducers } from  './orderSlice';
+export { elementsActions } from './elementsSlice';
+export { ingredientActions } from './ingredientSlice';
+export { ingredientsActions} from './ingredientsSlice';
+export { orderActions } from './orderSlice';
 
 export const rootReducer = combineReducers({
   ingredients: ingredientsReducers,
