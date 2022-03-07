63b57c0f2c1ecc30282600583ca0dd0e357ab108
PavelEgorov-ru <pkotofffff@yandex.ru>
2022-03-07 15:50:00 +0300

    импорты в оверлее

--- src/component/ModalOverlay/ModalOverlay.jsx
+++ src/component/ModalOverlay/ModalOverlay.jsx
@@ -1,14 +1,14 @@
 import PropTypes from 'prop-types';
-import styles from './ModalOverlay.module.css'
+import styles from './ModalOverlay.module.css';
 
 const ModalOverlay = ({onClose}) => {
   return (
     <div className={styles.container} onClick={onClose}></div>
   )
-}
+};
 
 ModalOverlay.propTypes = {
   onClose: PropTypes.func.isRequired,
-}
+};
 
-export default ModalOverlay
+export default ModalOverlay;
