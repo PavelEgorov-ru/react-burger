0b9d616ac9b346206087b0a7509a23a53d72cbab
PavelEgorov-ru <pkotofffff@yandex.ru>
2022-05-27 18:50:51 +0300

    защита авторизованного пользователя от страницы регистрации

--- src/component/ProtectedRoute/ProtectedRoute.jsx
+++ src/component/ProtectedRoute/ProtectedRoute.jsx
@@ -1,4 +1,4 @@
-import { Route, Redirect } from 'react-router';
+import { Route, Redirect, useLocation } from 'react-router';
 import { getCookie, setCookie } from '../../utils/cookie';
 import { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
@@ -6,6 +6,7 @@ import { fetchCheckUser, userActions } from '../../services/reducers';
 
 export function ProtectedRoute({ children, ...rest }) {
   const dispatch = useDispatch();
+  // const location = useLocation();
   const { isAuth, isLoader } = useSelector((store) => store.user);
 
   const auth = () => {
@@ -22,18 +23,24 @@ export function ProtectedRoute({ children, ...rest }) {
 
   if (!isLoader) return <div>загрузка данных</div>;
 
+  // if (isAuth) {
+  //   // return <Redirect to={location.state?.from || '/'} />;
+  // }
+
   return (
     isLoader && (
       <Route
         {...rest}
         render={({ location }) => {
+          console.log('локация', location);
           if (isAuth) {
+            // return location.state?.form ? <Redirect to="/" /> : children;
             return children;
           } else {
             return (
               <Redirect
                 to={{
-                  pathname: '/',
+                  pathname: '/login',
                   state: { from: location },
                 }}
               />
