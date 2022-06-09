09f7780a62188e1ebab5564a85e6e3741bc9b71e
PavelEgorov-ru <pkotofffff@yandex.ru>
2022-06-09 14:58:37 +0300

    шаблон контейнера с карточками заказов и сама карточка

---
+++ src/component/FeedCard/FeedCard.jsx
@@ -0,0 +1,7 @@
+import styles from './FeedCard.module.css';
+
+const FeedCard = () => {
+  return <div>Карточка заказа</div>;
+};
+
+export default FeedCard;
--- src/pages/feed/index.jsx
+++ src/pages/feed/index.jsx
@@ -1,5 +1,7 @@
 import styles from './feed.module.css';
+import { dataTest } from '../../utils/constants';
+import FeedList from '../../component/FeedList/FeedList';
 
 export const FeedPage = () => {
-  return <div>Лента заказов</div>;
+  return <FeedList />;
 };
