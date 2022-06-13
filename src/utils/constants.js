18cc7a5335d0e5a6f2a11a9ecd2a5f95f9e5fe23
PavelEgorov-ru <pkotofffff@yandex.ru>
2022-06-13 22:49:07 +0300

    подключение url для ws

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,5 @@
 export const BASE_URL = 'https://norma.nomoreparties.space/api';
+export const BASE_WS_URL = 'wss://norma.nomoreparties.space/orders/all';
 
 export const dataTest = {
   success: true,
