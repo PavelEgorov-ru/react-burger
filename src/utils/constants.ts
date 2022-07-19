6223d47ea60d831b1a7d8b75ea2eeb3bc01b04ca
PavelEgorov-ru <pkotofffff@yandex.ru>
2022-07-19 22:07:36 +0300

    типизация констант

---
+++ src/utils/constants.ts
@@ -0,0 +1,4 @@
+export const BASE_URL: 'https://norma.nomoreparties.space/api' =
+  'https://norma.nomoreparties.space/api';
+export const BASE_WS_URL: 'wss://norma.nomoreparties.space/orders' =
+  'wss://norma.nomoreparties.space/orders';
